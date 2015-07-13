e48041758f60107a9a3a60d2022484385f510eb5
znatz <z.nat.cn@gmail.com>
2015-07-13 12:42:11 +0900

    preparation for serving ios

--- app/controllers/EmployeesController.php
+++ app/controllers/EmployeesController.php
@@ -2,11 +2,6 @@
 
 class EmployeesController extends \BaseController {
 
-	/**
-	 * Display a listing of employees
-	 *
-	 * @return Response
-	 */
 	public function index()
 	{
 		$employees = Employee::all();
@@ -14,21 +9,11 @@ class EmployeesController extends \BaseController {
 		return View::make('employees.index', compact('employees'));
 	}
 
-	/**
-	 * Show the form for creating a new employee
-	 *
-	 * @return Response
-	 */
 	public function create()
 	{
 		return View::make('employees.create');
 	}
 
-	/**
-	 * Store a newly created employee in storage.
-	 *
-	 * @return Response
-	 */
 	public function store()
 	{
 		$validator = Validator::make($data = Input::all(), Employee::$rules);
@@ -43,12 +28,6 @@ class EmployeesController extends \BaseController {
 		return Redirect::route('employees.index');
 	}
 
-	/**
-	 * Display the specified employee.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function show($id)
 	{
 		$employee = Employee::findOrFail($id);
@@ -56,12 +35,6 @@ class EmployeesController extends \BaseController {
 		return View::make('employees.show', compact('employee'));
 	}
 
-	/**
-	 * Show the form for editing the specified employee.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function edit($id)
 	{
 		$employee = Employee::find($id);
@@ -69,12 +42,6 @@ class EmployeesController extends \BaseController {
 		return View::make('employees.edit', compact('employee'));
 	}
 
-	/**
-	 * Update the specified employee in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function update($id)
 	{
 		$employee = Employee::findOrFail($id);
@@ -91,12 +58,6 @@ class EmployeesController extends \BaseController {
 		return Redirect::route('employees.index');
 	}
 
-	/**
-	 * Remove the specified employee from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
 		Employee::destroy($id);
