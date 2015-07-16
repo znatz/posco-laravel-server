a3b703851240f14a9618471a8a4d10f102060d91
znatz <z.nat.cn@gmail.com>
2015-07-16 10:41:26 +0900

    append commit

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+class AuthController extends BaseController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function showSignUp()
+    {
+        return View::make('auth/signup');
+    }
+
+    public function execSignUp()
+    {
+        echo("runn");
+        $validation_rule = array(
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:4'
+        );
+
+        $validator = Validator::make(Input::all(), $validation_rule);
+
+        if ($validator->fails()) {
+            dd($validator->messages());
+            return Redirect::back()->withErrors($validator);
+        }
+
+        try {
+            Sentry::register(array(
+                'email' => Input::get('email'),
+                'password' => Input::get('password'),
+            ), true);
+            return Redirect::route('employee.index');
+        } catch (Exception $e) {
+            $this->messageBag->add('all', Lang::get('auth/message.signup.error'));
+        }
+        return Redirect::back()->withInput()->withErrors($this->messageBag);
+    }
+    public function showLogin()
+    {
+        return View::make('auth/login');
+    }
+    public function execLogin()
+    {
+        $validation_rule = array(
+            'email' => 'required|email',
+            'password' => 'required');
+        $validator = Validator::make(Input::all(), $validation_rule);
+        if($validator->fails())
+        {
+            return Redirect::back()->withInput()->withErrors($validator);
+        }
+        try{
+            $user = Sentry::authenticate(Input::only('email', 'password'), true);
+            Sentry::login($user, true);
+            return Redirect::route('employees.index');
+        }
+        catch (Cartalyst\Sentry\Throttling\UserSuspendedException $e) {
+            $this->messageBag->add('all', Lang::get('auth/message.account_suspended'));
+        }
+        catch (Cartalyst\Sentry\Throttling\UserBannedException $e) {
+            $this->messageBag->add('all', Lang::get('auth/message.account_banned'));
+        }
+        catch (Exception $e) {
+            $this->messageBag->add('all', Lang::get('auth/message.login.error'));
+        }
+        return Redirect::back()->withInput()->withErrors($this->messageBag);
+    }
+}
---
+++ app/lang/ja/auth/message.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'account_already_exists' => 'そのメールアドレスは既に使用されています',
+    'account_not_found'      => 'そのアカウントは存在しません。',
+    'account_not_activated'  => 'そのアカウントはアクティベートされていません。',
+    'account_suspended'      => 'そのアカウントは制限中です。',
+    'account_banned'         => 'そのアカウントは凍結されています。',
+    'signup'                 => array(
+        'error'              => 'アカウントの作成に失敗しました。',
+        'success'            => 'アカウントの作成に成功しました。'),
+    'login'                  => array(
+        'error'              => 'ログインに失敗しました。',
+        'success'            => 'ログインに成功しました。')
+);
