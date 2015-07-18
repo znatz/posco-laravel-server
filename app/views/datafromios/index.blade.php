ca8c9da9a5fa6e77ed2bf26755a3d56fb3261d96
znatz <z.nat.cn@gmail.com>
2015-07-19 08:42:29 +0900

    Tab icon added

--- app/views/datafromios/index.blade.php
+++ app/views/datafromios/index.blade.php
@@ -2,30 +2,39 @@
 @section('contents')
     <table class="table table-bordered text-center">
         <thead>
-        <tr class="row">
-            <td class="col-md-1">担当者ID</th>
-            <td class="col-md-3">商品名</th>
-            <td class="col-md-1">個数</th>
-            <td class="col-md-2">時間</th>
-            <td class="col-md-2">処理</th>
+        <tr class="row mark">
+            <td class="col-md-1">担当者ID
+            </th>
+            <td class="col-md-3">商品名
+            </th>
+            <td class="col-md-1">個数
+            </th>
+            <td class="col-md-2">時間
+            </th>
+            <td class="col-md-2">処理
+            </th>
         </tr>
         </thead>
         @foreach($datafromios as $d)
-        <tr class="row">
-            <td class="col-md-1">{{$d->tanto}}</td>
-            <td class="col-md-3">{{$d->goodsTitle}}</td>
-            <td class="col-md-1">{{$d->kosu}}</td>
-            <td class="col-md-2">{{$d->time}}</td>
-            <td class="col-md-2">{{Form::open(['route'=>'dataFromIOs.destroy', 'method'=>'DELETE'])}}
-                {{Form::text('id', $d->id, ['style'=>'display:none'])}}
-                {{Form::submit('削除', ['class' =>'btn btn-sm'])}}
-                {{Form::close()}}
-            </td>
-        </tr>
-            @endforeach
+            <tr class="row">
+                <td class="col-md-1">{{$d->tanto}}</td>
+                <td class="col-md-3">{{$d->goodsTitle}}</td>
+                <td class="col-md-1">{{$d->kosu}}</td>
+                <td class="col-md-2">{{$d->time}}</td>
+                <td class="col-md-2">{{Form::open(['route'=>'dataFromIOs.destroy', 'method'=>'DELETE'])}}
+                    {{Form::text('id', $d->id, ['style'=>'display:none'])}}
+                    {{Form::submit('削除', ['class' =>'btn btn-sm'])}}
+                    {{Form::close()}}
+                </td>
+            </tr>
+        @endforeach
     </table>
-    @stop
+@stop
 @section('contents2')
-    {{Form::open(['route'=>'dataFromIOs.clear', 'method'=>'post'])}}
-    {{Form::submit('記録をクリア')}}
-    @stop
+    <div class="col-md-5"></div>
+    <div class="col-md-2">
+        {{Form::open(['route'=>'dataFromIOs.clear', 'method'=>'post'])}}
+        {{Form::submit('全記録をクリア',['class'=>'form-control btn-primary btn-block btn-md'])}}
+    </div>
+    <div class="col-md-5"></div>
+@stop
